effc29157be56cd0e0e4d9bb8cdc2f8f1688a04a
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-10-15 22:16:46 +0530

    Time: 3 ms (71.98%), Space: 7.1 MB (17.64%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -12,10 +12,12 @@ public:
         
         // part string has been located
         // remove it
-        string left_part = s.substr(0, pos);
-        string right_part = s.substr(pos+part.size(), s.size());
 
-        s = left_part + right_part;  
+        // string left_part = s.substr(0, pos);
+        // string right_part = s.substr(pos+part.size(), s.size());
+        // s = left_part + right_part;  
+        
+        s.erase(pos, part.size());
         
 
         // RR
