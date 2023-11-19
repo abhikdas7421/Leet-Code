b0b249b86edfcdaef40b5a0563d1eb0375974f8e
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-11-20 00:51:19 +0530

    Time: 3 ms (71.91%), Space: 7.1 MB (43.12%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,23 +1,15 @@
 class Solution
 {
     public:
-        void removeOccurrencesRE(string &s, string &part)
-        {
 
-            if (s.find(part) == string::npos)
+        string removeOccurrences(string s, string part)
+        {
+            while (s.find(part) != string::npos)
             {
-                return;
+                int pos = s.find(part);
+                s.erase(pos, part.size());
             }
 
-            int pos = s.find(part);
-            s.erase(pos, part.length());
-
-            removeOccurrencesRE(s, part);
+            return s;
         }
-    string removeOccurrences(string s, string part)
-    {
-        removeOccurrencesRE(s, part);
-
-        return s;
-    }
 };
