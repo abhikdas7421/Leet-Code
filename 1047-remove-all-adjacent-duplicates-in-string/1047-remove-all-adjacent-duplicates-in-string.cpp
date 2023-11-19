e99da10fb3e2d7e7d78242357a727f26e5c005d0
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-11-20 00:35:32 +0530

    Time: 21 ms (48.88%), Space: 11.5 MB (40.06%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,22 +1,26 @@
-class Solution {
-public:
-    string removeDuplicates(string s) {
-        string ans = "";
-        int i = 0;
-        
-        while(i < s.length()) {
-            int lastIndex = ans.length()-1;
-            
-            if(lastIndex >= 0 && s[i] == ans[lastIndex]) {
-                ans.pop_back();
+class Solution
+{
+    public:
+        string removeDuplicates(string s)
+        {
+            string ans = "";
+            int i = 0;
+
+            while (i < s.length())
+            {
+                int lastIndex = ans.length() - 1;
+
+                if (lastIndex >= 0 && s[i] == ans[lastIndex])
+                {
+                    ans.pop_back();
+                }
+                else
+                {
+                    ans.push_back(s[i]);
+                }
+                i++;
             }
-            else {
-                ans.push_back(s[i]);
-            }
-            i++;
+
+            return ans;
         }
-        
-        return ans;
-        
-    }
 };
