1eb78afe11f86c7b70f7fa548ff351d1be9e0257
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-11-20 19:34:24 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (52.94%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.cpp
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -1,32 +1,39 @@
-class Solution {
-public:
-    bool isLetters(char ch) {
-        if((ch >= 'a' && 'z' >= ch) || (ch >= 'A' && 'Z' >= ch)) {
-            return true;
+class Solution
+{
+    public:
+        bool isLetters(char ch)
+        {
+            if ((ch >= 'a' && 'z' >= ch) || (ch >= 'A' && 'Z' >= ch))
+            {
+                return true;
+            }
+
+            return false;
         }
-        
-        return false;
-    }
-    
-    string reverseOnlyLetters(string s) {
-        int i = 0, j = s.size()-1;
-        
-        while(i < j) {
-            
-            if(isLetters(s[i]) && isLetters(s[j])) {
-                swap(s[i], s[j]);
-                i++;
-                j--;
+
+        string reverseOnlyLetters(string s)
+        {
+            int i = 0, j = s.size() - 1;
+
+            while (i < j)
+            {
+
+                if (isLetters(s[i]) && isLetters(s[j]))
+                {
+                    swap(s[i], s[j]);
+                    i++;
+                    j--;
+                }
+                else if (!isLetters(s[i]))
+                {
+                    i++;
+                }
+                else if (!isLetters(s[j]))
+                {
+                    j--;
+                }
             }
-            else if(!isLetters(s[i])) {
-                i++;
-            }
-            else if(!isLetters(s[j])) {
-                j--;
-            }
+
+            return s;
         }
-        
-        return s;
-        
-    }
 };
