dce0068d87cf95c7b55981eaddbf22a089021fea
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-11-20 19:33:33 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (52.94%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isLetters(char ch) {
+        if((ch >= 'a' && 'z' >= ch) || (ch >= 'A' && 'Z' >= ch)) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    string reverseOnlyLetters(string s) {
+        int i = 0, j = s.size()-1;
+        
+        while(i < j) {
+            
+            if(isLetters(s[i]) && isLetters(s[j])) {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+            else if(!isLetters(s[i])) {
+                i++;
+            }
+            else if(!isLetters(s[j])) {
+                j--;
+            }
+        }
+        
+        return s;
+        
+    }
+};
