0989a1d5f93fcbcbb8415ea7d00d07b1044c0f52
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-11-20 19:19:37 +0530

    Time: 8 ms (62.64%), Space: 7.8 MB (45.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,31 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        
-        if(s.size() != t.size()) return false;
-        
-        vector<int> freqTable(26, 0);
-        
-        for(int i = 0; i < s.size(); i++) {
-            freqTable[s[i] - 'a']++;
-        }
-        
-        for(int i = 0; i < t.size(); i++) {
-            freqTable[t[i] - 'a']--;
-        }
-        
-        for(int i = 0; i < 26; i++) {
-            if(freqTable[i] != 0) {
-                return false;
+class Solution
+{
+    public:
+        bool isAnagram(string s, string t)
+        {
+
+            if (s.size() != t.size()) return false;
+
+            vector<int> freqTable(26, 0);
+
+            for (int i = 0; i < s.size(); i++)
+            {
+                freqTable[s[i] - 'a']++;
             }
+
+            for (int i = 0; i < t.size(); i++)
+            {
+                freqTable[t[i] - 'a']--;
+            }
+
+            for (int i = 0; i < 26; i++)
+            {
+                if (freqTable[i] != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
-        
-        return true;
-        
-        
-    }
 };
