03dfe933eb5db98d0566eaf3194ec68435e42a65
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-11-20 12:19:21 +0530

    Time: 15 ms (29.02%), Space: 14.1 MB (23.61%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -4,11 +4,11 @@ class Solution
         int toMinute(string time)
         {
             // using in built function of ---> string to int
-            int h = stoi(time.substr(0, 2));
-            int m = stoi(time.substr(3, 2));
+            // int h = stoi(time.substr(0, 2));
+            // int m = stoi(time.substr(3, 2));
             
-            // int h = ((time[0] - '0') *10) + (time[1] - '0');
-            // int m = ((time[3] - '0') *10) + (time[4] - '0');
+            int h = ((time[0] - '0') *10) + (time[1] - '0');
+            int m = ((time[3] - '0') *10) + (time[4] - '0');
 
             int totalMinute = h *60 + m;
             
