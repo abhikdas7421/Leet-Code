c5e632213e31f9c7de870549ea63221ce3d6e07d
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-11-22 10:54:15 +0530

    Time: 11 ms (52.89%), Space: 10.8 MB (46.62%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,52 @@
+class Solution
+{
+    public:
+        string convert(string s, int numRows)
+        {
+            if(numRows == 1) return s;
+            
+            string ans = "";
+            vector<string> p(numRows);
+
+            bool topToBottom = true;
+
+            int i = 0;
+            int j = 0;
+            while (i < s.size())
+            {
+                if (topToBottom)
+                {
+                    if (j < numRows)
+                    {
+                        p[j].push_back(s[i]);
+                        j++;
+                        i++;
+                    }
+                    else
+                    {
+                        j -= 2;
+                        topToBottom = false;
+                    }
+                }
+                else
+                {
+                    if (j >= 0)
+                    {
+                        p[j].push_back(s[i]);
+                        j--;
+                        i++;
+                    }
+                    else
+                    {
+                        j += 2;
+                        topToBottom = true;
+                    }
+                }
+            }
+            for(int i = 0; i < numRows; i++) {
+                ans += p[i];
+            }
+            
+            return ans;
+        }
+};
