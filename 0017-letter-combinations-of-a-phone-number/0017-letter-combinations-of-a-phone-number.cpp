498cfe21dc34454c22f073962bf8fe40b98b9d28
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-12-07 15:40:09 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (95.12%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,7 +2,7 @@ class Solution
 {
     public:
     
-        void solve(string digits, int index, vector<string> &mapping, vector<string> &ans, string output) 
+        void solve(string &digits, int index, vector<string> &mapping, vector<string> &ans, string output) 
         {
             // base case
             if(index >= digits.length())
