9d87bac4a8429380a125831db7bdd290635cc6e3
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-12-20 21:41:42 +0530

    Time: 105 ms (5.20%), Space: 68 MB (56.58%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maxSubArrayHelper(vector<int> &nums, int start, int end) {
+        // base case
+        if(start == end) return nums[start];
+        
+        int maxLeftBorderSum = INT_MIN, maxRightBorderSum = INT_MIN;
+        int mid = start + (end - start)/2;
+        
+        int maxLeftSum = maxSubArrayHelper(nums, start, mid);
+        int maxRightSum = maxSubArrayHelper(nums, mid+1, end);
+        
+        // Cross border sum
+        int leftBorderSum = 0, rightBorderSum = 0;
+        for(int i = mid; i >= start; i--) {
+            leftBorderSum += nums[i];
+            
+            if(leftBorderSum > maxLeftBorderSum) {
+                 maxLeftBorderSum = leftBorderSum;
+            }
+               
+        }
+        
+        for(int i = mid+1; i <= end; i++) {
+            rightBorderSum += nums[i];
+            
+            if(rightBorderSum > maxRightBorderSum) {
+                maxRightBorderSum = rightBorderSum;
+            }
+        }
+        
+        int crossBorderSum = maxLeftBorderSum + maxRightBorderSum;
+        
+        return max(crossBorderSum, max(maxLeftSum, maxRightSum));
+    }
+    
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArrayHelper(nums, 0, nums.size()-1);
+    }
+};
