c5d78d400015339aa34d2190433716cb0c578ac5
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-12-20 12:32:04 +0530

    Time: 182 ms (60.46%), Space: 69.1 MB (60.20%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,50 +1,60 @@
-class Solution {
-public:
-    void merge(vector<int> &nums, vector<int> &temp, int s, int mid, int e) {
-        int i = s, j = mid+1, k = s;
-        
-        while(i <= mid && j <= e) {
-            if(nums[i] <= nums[j]) {
+class Solution
+{
+    public:
+        void merge(vector<int> &nums, vector<int> &temp, int s, int mid, int e)
+        {
+            int i = s, j = mid + 1, k = s;
+
+            while (i <= mid && j <= e)
+            {
+                if (nums[i] <= nums[j])
+                {
+                    temp[k++] = nums[i++];
+                }
+                else
+                {
+                    temp[k++] = nums[j++];
+                }
+            }
+
+            while (i <= mid)
+            {
                 temp[k++] = nums[i++];
             }
-            else {
+
+            while (j <= e)
+            {
                 temp[k++] = nums[j++];
             }
+
+           	// copy temp array to nums array;
+            while (s <= e)
+            {
+                nums[s] = temp[s];
+                ++s;
+            }
         }
-        
-        while(i <= mid) {
-            temp[k++] = nums[i++];
+
+        void mergeSort(vector<int> &nums, vector<int> &temp, int s, int e)
+        {
+            // base case
+            if (s >= e)
+            {
+                return;
+            }
+
+            int mid = s + (e - s) / 2;
+
+            mergeSort(nums, temp, s, mid);
+            mergeSort(nums, temp, mid + 1, e);
+
+            merge(nums, temp, s, mid, e);
         }
-        
-        while(j <= e) {
-            temp[k++] = nums[j++];
+
+        vector<int> sortArray(vector<int> &nums)
+        {
+            vector<int> temp(nums.size(), 0);
+            mergeSort(nums, temp, 0, nums.size() - 1);
+            return nums;
         }
-        
-        // copy temp array to nums array;
-        while(s <= e) {
-            nums[s] = temp[s];
-            ++s;
-        }
-    }
-    
-    void mergeSort(vector<int> &nums, vector<int> &temp, int s, int e) {
-        // base case
-        if(s >= e) {
-            return;
-        }
-        
-        int mid = s + (e-s)/2;
-        
-        mergeSort(nums, temp, s, mid);
-        mergeSort(nums, temp, mid+1, e);
-        
-        merge(nums, temp, s, mid, e);
-    }
-    
-    vector<int> sortArray(vector<int>& nums) {
-        vector<int> temp(nums.size(), 0);
-        mergeSort(nums, temp, 0, nums.size()-1);
-        return nums;
-        
-    }
 };
