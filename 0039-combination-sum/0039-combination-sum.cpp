7b7df0387a89f1a6cbd85062b7aa8e315636c361
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-12-21 12:57:45 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (83.64%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+        void combinationSumHelper(vector<int> &candidates, int target, int index, vector<vector<int>> &combn, vector<int> &v) {
+            if(target == 0) {
+               combn.push_back(v);
+                
+                
+                return;
+            }
+            
+            
+            for(int i = index; i < candidates.size(); i++) {
+                if(target-candidates[i] >= 0) {
+                    
+                    v.push_back(candidates[i]);
+                    
+                    combinationSumHelper(candidates, target-candidates[i], i, combn, v);
+                    
+                    // backtracking
+                    v.pop_back();
+                    
+                }
+            }
+        } 
+    
+        vector<vector < int>> combinationSum(vector<int> &candidates, int target) {
+            vector<vector<int>> combn;
+            vector<int> v;
+            combinationSumHelper(candidates, target, 0, combn, v);
+            
+            
+            return combn;
+            
+            
+        }
+};
