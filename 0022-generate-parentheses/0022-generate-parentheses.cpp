65f13c752d0c805c8d0a0ba259ed6c9dc515c4dc
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-12-24 16:21:29 +0530

    Time: 0 ms (100.00%), Space: 11.8 MB (81.75%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,36 +1,35 @@
-class Solution
-{
-    public:
-        void solve(vector<string> &ans, int n, int open, int close, string str)
-        {
-           	// base case
-            if (open == 0 && close == 0)
-            {
-                ans.push_back(str);
-                return;
-            }
-
-           	// include open "("
-            if (open > 0)
-            {
-                solve(ans, n, open - 1, close, str + '(');
-            }
-
-           	// include close ")"
-            if (open < close)
-            {
-                solve(ans, n, open, close - 1, str + ')');
-            }
+class Solution {
+public:
+    void solve(vector<string> &ans, int open, int close, int n, string &str) {
+        // base case
+        if(open == 0 && close == 0) {
+            ans.push_back(str);
+            return;
         }
-    vector<string> generateParenthesis(int n)
-    {
+        
+        // include opening parentheses
+        if(open > 0) {
+            str.push_back('(');
+            solve(ans, open-1, close, n, str);
+            str.pop_back();
+        }
+        
+        
+        // include closing parentheses
+        if(close > open) {
+            str.push_back(')');
+            solve(ans, open, close-1, n, str);
+            str.pop_back();
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
         vector<string> ans;
         int open = n;
         int close = n;
         string str = "";
-
-        solve(ans, n, open, close, str);
-
+        
+        solve(ans, open, close, n, str);
         return ans;
     }
 };
