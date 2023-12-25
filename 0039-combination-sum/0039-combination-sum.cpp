bf2e3367d26df072fe9f35e9e555400ecdc6ec08
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2023-12-25 16:21:58 +0530

    Time: 3 ms (85.27%), Space: 11.1 MB (68.96%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,37 +1,44 @@
 class Solution
 {
     public:
-        void combinationSumHelper(vector<int> &candidates, int target, int index, vector<vector<int>> &combn, vector<int> &v) {
-            if(target == 0) {
-               combn.push_back(v);
-                
-                
+        void combinationSumHelper(vector<int> &candidates, int target, int index, vector<vector< int >> &combn, vector< int > &v)
+        {
+            if (target == 0)
+            {
+                combn.push_back(v);
+
                 return;
             }
-            
-            
-            for(int i = index; i < candidates.size(); i++) {
-                if(target-candidates[i] >= 0) {
-                    
-                    v.push_back(candidates[i]);
-                    
-                    combinationSumHelper(candidates, target-candidates[i], i, combn, v);
-                    
-                    // backtracking
-                    v.pop_back();
-                    
-                }
+
+            for (int i = index; i < candidates.size(); i++)
+            {
+               	//                 if(target-candidates[i] >= 0) {
+
+               	//                     v.push_back(candidates[i]);
+
+               	//                     combinationSumHelper(candidates, target-candidates[i], i, combn, v);
+
+               	//                    	// backtracking
+               	//                     v.pop_back();
+
+               	//                 }
+
+                if(candidates[i] > target) break;
+
+                v.push_back(candidates[i]);
+                combinationSumHelper(candidates, target - candidates[i], i, combn, v);
+               	// backtracking
+                v.pop_back();
             }
-        } 
-    
-        vector<vector < int>> combinationSum(vector<int> &candidates, int target) {
-            vector<vector<int>> combn;
-            vector<int> v;
-            combinationSumHelper(candidates, target, 0, combn, v);
-            
-            
-            return combn;
-            
-            
         }
+
+    vector<vector < int>> combinationSum(vector<int> &candidates, int target)
+    {
+        sort(candidates.begin(), candidates.end());
+        vector<vector < int>> combn;
+        vector<int> v;
+        combinationSumHelper(candidates, target, 0, combn, v);
+
+        return combn;
+    }
 };
