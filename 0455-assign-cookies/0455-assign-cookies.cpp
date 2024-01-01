14cd246e0f30cfd2ac9e8ae79941ce7d8daf61e3
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-01-01 20:52:56 +0530

    Time: 52 ms (7.63%), Space: 17.8 MB (36.11%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        
+        if(s.size() == 0) return 0;
+        
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
         
