888e2ed0c32d92bbbd3d803607190b59656cb758
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-01-04 10:45:27 +0530

    Time: 117 ms (71.64%), Space: 85.2 MB (46.04%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int minOperation = 0;
         unordered_map<int, int> freq;
         for(int i = 0; i < nums.size(); i++) {
             freq[nums[i]]++;
         }
         
+        int minOperation = 0;
         for(auto it:freq) {
-            int ele = it.first;
             int eleCount = it.second;
+            if(eleCount== 1) return -1;
             
-            while(eleCount > 1) {
-                if(eleCount == 3 || eleCount > 4) {
-                    freq[ele] = freq[ele]-3;
-                    eleCount -= 3;
-                    ++minOperation;
-                }
-                else if(eleCount >= 2) {
-                    freq[ele] = freq[ele]-2;
-                    eleCount -= 2;
-                    ++minOperation;
-                }
-            }
-            
-            if(eleCount == 1) {
-                return -1;
+            minOperation += (eleCount/3);
+            if(eleCount%3) {
+                minOperation++;
             }
         }
         
