345e020f84624fed7b1b57df5f15466884fb09cc
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-01-05 12:22:25 +0530

    Time: 3 ms (98.69%), Space: 10.7 MB (92.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,50 +1,41 @@
 class Solution {
 public:
-    
-    int solve(vector<int> &nums, int curr, int prev) {
-        if(curr >= nums.size()) {
-            return 0;
+    int bs(vector<int> &nums, int target) {
+        int s = 0, e = nums.size()-1;
+        
+        int index = -1;
+        
+        while(s <= e) {
+            int m = s + (e-s)/2;
+            
+            if(nums[m] >= target) {
+                index = m;
+                e = m-1;
+            }
+            else if(nums[m] < target) {
+                s = m+1;
+            }
         }
         
-        // pick
-        int take = 0;
-        if(prev == -1 || nums[curr] > nums[prev]) {
-            take = 1+solve(nums, curr+1, curr);
-        }
-        
-        // not pick
-        int notTake = solve(nums, curr+1, prev);
-        
-        return max(take, notTake);
+        return index;
     }
     
-    int solveMem(vector<int> &nums, int curr, int prev, vector<vector<int>> &dp) {
-        if(curr >= nums.size()) {
-            return 0;
-        }
-        
-        if(dp[curr][prev+1] != -1) {
-            return dp[curr][prev+1];
-        }
-        
-        // pick
-        int take = 0;
-        if(prev == -1 || nums[curr] > nums[prev]) {
-            take = 1+solveMem(nums, curr+1, curr, dp);
-        }
-        
-        // not pick
-        int notTake = solveMem(nums, curr+1, prev, dp);
-        
-        dp[curr][prev+1] = max(take, notTake);
-        return dp[curr][prev+1];
-        
-    }
-   
     int lengthOfLIS(vector<int>& nums) {
-        
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return solveMem(nums, 0, -1, dp);
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > ans.back()) {
+                ans.push_back(nums[i]);
+            }
+            else {
+                int index = bs(ans, nums[i]);
+                ans[index] = nums[i];
+            }
+        }
+        
+        return ans.size();
+        
     }
 };
