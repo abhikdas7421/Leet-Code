19a5b9927fa5602008376be0b437bbee35885d37
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-01-15 01:11:25 +0530

    Time: 62 ms (94.58%), Space: 20.7 MB (96.49%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.size() != word2.size()) return false;
+        if(word1.size() != word2.size()) {
+            return false;
+        }
         
-        int n = word1.size();
-        
-        unordered_map<char, int> mp1;
-        unordered_map<char, int> mp2;
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
         
         for(auto ch : word1) {
-            mp1[ch]++;
+            freq1[ch-'a']++;
         }
         for(auto ch : word2) {
-            mp2[ch]++;
+            freq2[ch-'a']++;
         }
         
-        for(int i = 0; i < n; i++) {
-            char ch1 = word1[i];
-            char ch2 = word2[i];
-            
-            if(mp1.find(ch2) == mp1.end() || mp2.find(ch1) == mp2.end()) {
+        for(int i = 0; i < 26; i++) {
+            if((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
                 return false;
             }
         }
         
-        vector<int> v1;
-        vector<int> v2;
+        sort(freq1.begin(), freq1.end());
+        sort(freq2.begin(), freq2.end());
         
-        for(auto it : mp1) {
-            v1.push_back(it.second);
-        }
-        
-        for(auto it : mp2) {
-            v2.push_back(it.second);
-        }
-        
-        sort(v1.begin(), v1.end());
-        sort(v2.begin(), v2.end());
-        
-        return v1 == v2;
-        
+        return freq1 == freq2;
     }
 };
