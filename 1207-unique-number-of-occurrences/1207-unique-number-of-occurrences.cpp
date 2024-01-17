d2fcb8a083e4f5458e7b2f5cef5d603e0d48c741
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-01-17 11:32:23 +0530

    Time: 5 ms (39.16%), Space: 8.3 MB (99.93%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> mp;
+        sort(arr.begin(), arr.end());
         
-        for(auto v : arr) {
-            mp[v]++;
-        }
+        vector<int> freq;
         
-        vector<int> v;
-        for(auto it:mp) {
-            v.push_back(it.second);
-        }
-        
-        sort(v.begin(), v.end());
-        
-        for(int i = 0; i < v.size()-1; i++) {
+        for(int i = 0; i < arr.size(); i++) {
+            int count = 1;
             
-            if(v[i] == v[i+1]){
+            while(i+1 < arr.size() && arr[i] == arr[i+1]) {
+                count++;
+                i++;
+            }
+            
+            freq.push_back(count);
+            
+        }
+        
+        sort(freq.begin(), freq.end());
+        
+        for(int i = 0; i < freq.size()-1; i++) {
+            if(freq[i] == freq[i+1]) {
                 return false;
             }
         }
         
         return true;
+        
     }
 };
