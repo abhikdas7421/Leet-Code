3825d26698b1da2de6db44c53465c84d766f2f8a
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-01-18 18:20:30 +0530

    Time: 11 ms (21.07%), Space: 12.1 MB (5.56%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -36,4 +36,38 @@ public:
         return solve(head, mp);
         
     }
+    
+    //Approach 2:- Iterative Approach
+    /* Node* copyRandomList(Node* head) {
+        // dummy node
+        Node *newHead = new Node(-1);
+        Node* newTail = newHead;
+        
+        unordered_map<Node*, Node*> mp;
+        
+        Node* temp = head;
+        while(temp != 0) {
+            newTail->next = new Node(temp->val);
+            newTail = newTail->next;
+            
+            mp[temp] = newTail;
+            temp = temp->next;
+        }
+        
+        // remove dummy node;
+        Node* dummy = newHead;
+        newHead = newHead->next;
+        dummy->next = 0;
+        
+        temp = head;
+        Node* newTemp = newHead;
+        while(temp != 0) {
+            newTemp->random = mp[temp->random];
+            temp = temp->next;
+            newTemp = newTemp->next;
+        }
+        
+        return newHead;
+    }
+    */
 };
