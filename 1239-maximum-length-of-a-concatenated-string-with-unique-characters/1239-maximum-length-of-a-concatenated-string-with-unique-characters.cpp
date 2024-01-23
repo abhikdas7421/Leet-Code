028e5b54c4b3efb15d5179221e34f7e4d3f5d055
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-01-23 14:01:27 +0530

    Time: 56 ms (54.29%), Space: 58.4 MB (47.47%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    bool isUnique(string str) {
-        // sort(str.begin(), str.end());
-        vector<bool> seen(26, false);
-        // unordered_set<char> seen;
-        for(int i = 0; i < str.length(); i++) {
-            // if(str[i] == str[i-1]) {
-            //     return false;
-            // }
-            if(seen[str[i]-'a']) {
+    bool isUnique(string str1, string str2) {
+        vector<int> freq(26, 0);
+        
+        for(auto ch : str1) {
+            if(freq[ch-'a']) {
                 return false;
             }
-            seen[str[i]-'a'] = true;
+            freq[ch-'a']++;
         }
+        
+        for(auto ch : str2) {
+            if(freq[ch-'a']) {
+                return false;
+            }
+            freq[ch-'a']++;
+        }
+        
         return true;
     }
     
+    /*
     void solve(vector<string>& arr, int i, string& subSeq, int& ans) {
         // base case
         if(i >= arr.size()) {
@@ -44,13 +49,30 @@ public:
         
        
     }
+    */
+    
+    int solve(vector<string> &arr, int i, string temp) {
+        
+        if(i >= arr.size()) {
+            return temp.length();
+        }
+        
+        int include = 0;
+        int exclude = 0;
+        
+        exclude = solve(arr, i+1, temp);
+        if(isUnique(temp, arr[i])) {
+            include = solve(arr, i+1, temp+arr[i]);
+        }
+        
+        return max(include, exclude);
+        
+    }
     
     int maxLength(vector<string>& arr) {
-        string subSeq;
-        int ans = 0;
-        solve(arr, 0, subSeq, ans);
-        cout << "Ans: " <<  ans;
+        string temp = "";
         
+        int ans = solve(arr, 0, temp);
         return ans;
     }
 };
