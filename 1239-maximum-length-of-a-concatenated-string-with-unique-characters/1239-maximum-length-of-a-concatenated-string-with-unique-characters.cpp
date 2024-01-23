9a51fc97cb86fbfb6500718434f08b69576763db
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-01-23 14:07:28 +0530

    Time: 99 ms (44.88%), Space: 82.6 MB (27.30%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -19,42 +19,16 @@ public:
         
         return true;
     }
-    
-    /*
-    void solve(vector<string>& arr, int i, string& subSeq, int& ans) {
-        // base case
-        if(i >= arr.size()) {
-            if(subSeq.size() > ans) {
-                ans = subSeq.size();
-                
-                // cout << ans << endl;
-                // cout << subSeq << endl;
-            }
-            return;
-        }
-        
-        // not pick 
-        solve(arr, i+1, subSeq, ans);
-        
-        // pick
-        
-        string temp = subSeq+arr[i];
-        if(isUnique(temp)) {
-            solve(arr, i+1, temp, ans);
-            
-        }
-        else {
-            solve(arr, i+1, subSeq, ans);
-        }
-        
-       
-    }
-    */
-    
+    unordered_map<string, int> mp;
     int solve(vector<string> &arr, int i, string temp) {
         
         if(i >= arr.size()) {
-            return temp.length();
+            mp[temp] = temp.length();
+            return mp[temp];
+        }
+        
+        if(mp.find(temp) != mp.end()) {
+            return mp[temp];
         }
         
         int include = 0;
@@ -65,13 +39,13 @@ public:
             include = solve(arr, i+1, temp+arr[i]);
         }
         
-        return max(include, exclude);
-        
+        mp[temp] = max(include, exclude);
+        return mp[temp];
     }
     
     int maxLength(vector<string>& arr) {
         string temp = "";
-        
+        mp.clear();
         int ans = solve(arr, 0, temp);
         return ans;
     }
