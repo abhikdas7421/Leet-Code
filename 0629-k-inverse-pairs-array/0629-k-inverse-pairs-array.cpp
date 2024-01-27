39c524159702c0330c82bec76cdc1bf958ef4bed
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-01-27 12:54:29 +0530

    Time: 2005 ms (8.79%), Space: 17.5 MB (10.99%) - LeetHub

---
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -0,0 +1,22 @@
+#define mod 1000000007
+class Solution {
+public:
+    
+    int solve(int n, int k, vector<vector<int>> &dp) {
+        if(n == 0) return 0;
+        if(k == 0) return 1;
+        
+        if(dp[n][k] != -1) return dp[n][k];
+        
+        int count = 0;
+        for(int i = 0; i <= min(k, n-1); i++) {
+            count = (count + solve(n-1, k-i, dp))%mod;
+        }
+        
+        return dp[n][k] = count;
+    }
+    int kInversePairs(int n, int k) {
+        vector<vector<int>> dp(n+1, vector<int>(k+1, -1));
+        return solve(n, k, dp);
+    }
+};
