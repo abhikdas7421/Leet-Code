a0e0457a289d9a64c1486442e3c0a66e97ef5cdd
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-06 20:31:15 +0530

    Time: 31 ms (55.30%), Space: 24.5 MB (20.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,59 +1,20 @@
-class Solution
-{
-    public:
-        // Method - 1
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
         
-        vector<vector < string>> groupAnagrams(vector<string> &strs)
-        {
-            map<string, vector < string>> mp;
-
-            for (auto str: strs)
-            {
-                string s = str;
-                sort(s.begin(), s.end());
-                mp[s].push_back(str);
-            }
-
-            vector<vector < string>> ans;
-            for (auto it = mp.begin(); it != mp.end(); it++)
-            {
-                ans.push_back(it->second);
-            }
-
-            return ans;
+        for(auto str : strs) {
+            string s = str;
+            sort(s.begin(), s.end());
+            mp[s].push_back(str);
         }
-
-        // Method - 2;
-        /*
-        vector<int> hash(string s)
-        {
-            vector<int> hash(26, 0);
-
-            for (int i = 0; i < s.size(); i++)
-            {
-                hash[s[i] - 'a']++;
-            }
-
-            return hash;
+        
+        vector<vector<string>> ans;
+        
+        for(auto it : mp) {
+            ans.push_back(it.second);
         }
-
-        vector<vector < string>> groupAnagrams(vector<string> &strs)
-        {
-            map<vector < int>, vector< string>> mp;
-
-            for (auto str: strs)
-            {
-                mp[hash(str)].push_back(str);
-            }
-
-            vector<vector < string>> ans;
-
-            for (auto it = mp.begin(); it != mp.end(); it++)
-            {
-                ans.push_back(it->second);
-            }
-
-            return ans;
-        }
-        */
+        
+        return ans;
+    }
 };
