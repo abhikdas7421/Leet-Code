5a71fc3ca6afada9fa33bc958852caec22c0ab1e
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-08 16:27:39 +0530

    Time: 102 ms (81.54%), Space: 130.5 MB (33.78%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        
+        // petrol remaning
+        int balance = 0;
+        // petrol kam pada
+        int deficit = 0;
+        // circuti starting point
+        int start = 0;
+        
+        for(int i = 0; i < gas.size(); i++) {
+            balance += gas[i]-cost[i];
+            
+            if(balance < 0) {
+                deficit += balance;
+                balance = 0;
+                start = i+1;
+            }
+        }
+        
+        if(deficit+balance >= 0) {
+            return start;
+        }
+        return -1;
+    }
+};
