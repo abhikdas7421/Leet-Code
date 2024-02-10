ceb7264d19d1dc31fd6f79190c9c5ae0c9437c3f
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-10 09:07:28 +0530

    Time: 4 ms (76.20%), Space: 7.4 MB (74.15%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    int expandAlongIndex(string s, int i, int j) {
-        int count = 0;
-
+    int expandAlongIndex(string &s, int i, int j) {
+        int ans = 0;
         
-        while((i >= 0) && (j <= s.size()-1) && (s[i] == s[j])) {
-            count++;
+        while(i >= 0 && j < s.length()) {
+            
+            if(s[i] != s[j]) {
+                break;
+            }
+            ans++;
             i--;
             j++;
         }
         
-        return count;
+        return ans;
+        
     }
-    
     int countSubstrings(string s) {
         
-        int count = 0;
+        int ans = 0;
         for(int i = 0; i < s.length(); i++) {
+            int odd = expandAlongIndex(s, i, i);
             
-            int oddCount = expandAlongIndex(s, i, i);
-            int evenCount = expandAlongIndex(s, i, i+1);
+            int even = expandAlongIndex(s, i, i+1);
             
-            count += (oddCount + evenCount);
+            ans += odd+even;
         }
         
-        return count;
-        
+        return ans;
     }
 };
