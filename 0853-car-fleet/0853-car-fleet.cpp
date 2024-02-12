abc39c06647c20d86ff19681c3839eb196498c4d
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-12 22:31:29 +0530

    Time: 146 ms (68.25%), Space: 82.2 MB (45.62%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    static bool myCmp(pair<int, int> &p1, pair<int, int> &p2) {
+        return p1.first < p2.first;
+    }
+    
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        
+        vector<pair<int, int>> car;
+        for(int i = 0; i < n; i++) {
+            pair<int, int> p = make_pair(position[i], speed[i]);
+            car.push_back(p);
+        }
+        sort(car.begin(), car.end(), myCmp);
+        
+        stack<float> st;
+        for(int i = 0; i < n; i++) {
+            int pos = car[i].first;
+            int speed = car[i].second;
+            float time = (float)(target-pos)/speed;
+            
+            while(!st.empty() && st.top() <= time) {
+                st.pop();
+            }
+            st.push(time);
+        }
+        
+        int noOfFleet = st.size();
+        return noOfFleet;
+    }
+};
