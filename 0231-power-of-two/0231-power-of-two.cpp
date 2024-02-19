ae93a20ac1818600e332aea1f6395ef3080be0dd
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-20 00:44:31 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (19.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) return false;
+        // Approach 2:- Bit Magic
+        /*if(n <= 0) return false;
         
         if((n & n-1) == 0) {
             return true;
         }
-        return false;
+        return false;*/
+        
+        // Approch 3:- built in function
+        if(n <= 0) return false;
+        int count = __builtin_popcount(n);
+        
+        return (count == 1) ? true : false;
     }
 };
