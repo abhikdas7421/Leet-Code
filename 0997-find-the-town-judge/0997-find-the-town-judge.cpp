4703c5bbc0d3bfc53fe1158d3198441137429204
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-23 01:20:30 +0530

    Time: 123 ms (81.41%), Space: 72.5 MB (21.01%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int> ans(n+1, 0);
+        
         for(auto it : trust) {
             ans[it[0]] = -1;
             
@@ -14,15 +15,20 @@ public:
         int maxTrust = INT_MIN;
         int index = -1;
         for(int i = 1; i <= n; i++) {
-            if(ans[i] > maxTrust) {
-                maxTrust = ans[i];
-                index = i;
+            // if(ans[i] > maxTrust) {
+            //     maxTrust = ans[i];
+            //     index = i;
+            // }
+            if(ans[i] == n-1) {
+                return i;
             }
         }
         
-        if(maxTrust == n-1) {
-            return index;
-        }
         return -1;
+        
+        // if(maxTrust == n-1) {
+        //     return index;
+        // }
+        // return -1;
     }
 };
