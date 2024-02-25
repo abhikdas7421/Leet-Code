3e981eb3056a457b8ccf74dc898cede1ec985cae
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-25 11:30:21 +0530

    Time: 281 ms (91.48%), Space: 97.2 MB (99.75%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        int m = meetings.size();
+        
+        sort(meetings.begin(), meetings.end());
+        
+        vector<long long> lastAvailableAt(n, 0);
+        
+        vector<int> roomUsedCount(n, 0);
+        
+        for(vector<int> &meet : meetings) {
+            
+            int start = meet[0];
+            int end = meet[1];
+            int duration = end-start;
+            
+            bool found = false;
+            
+            long long earlyEndRoomTime = LLONG_MAX;
+            int earlyEndRoom = 0;
+            for(int room = 0; room < n; room++) {
+                
+                if(lastAvailableAt[room] <= start) {
+                    lastAvailableAt[room] = end;
+                    roomUsedCount[room]++;
+                    found = true;
+                    break;
+                }
+                
+                if(lastAvailableAt[room] < earlyEndRoomTime) {
+                    earlyEndRoomTime = lastAvailableAt[room];
+                    earlyEndRoom = room;
+                }
+            }
+            
+            if(!found) {
+                lastAvailableAt[earlyEndRoom] += duration;
+                roomUsedCount[earlyEndRoom]++;
+            }
+        }
+        
+        int resultRoom = -1;
+        int maxUse = 0;
+        
+        for(int room = 0; room < n; room++) {
+            // cout << roomUsedCount[room] << " ";
+            if(roomUsedCount[room] > maxUse) {
+                resultRoom = room;
+                maxUse = roomUsedCount[room];
+            }
+        }
+        // cout<<endl;
+        return resultRoom;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
