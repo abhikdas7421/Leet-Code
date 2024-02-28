a763154699b2b56163e9e8a01c11eab201812527
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-28 09:06:28 +0530

    Time: 9 ms (54.31%), Space: 20.1 MB (76.07%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -11,7 +11,8 @@
  */
 class Solution {
 public:
-    int findBottomLeftValue(TreeNode* root) {
+    // Approach 1:- (Using BFS)
+    /*int findBottomLeftValue(TreeNode* root) {
         
         queue<TreeNode*> que;
         que.push(root);
@@ -39,5 +40,30 @@ public:
         }
         
         return ans;
+    }*/
+    
+    // Approach 2:- Using DFS
+    void solve(TreeNode* node, int currDepth, int &depth, int &leftMostValue) {
+        if(node == NULL) {
+            return;
+        }
+        
+        if(currDepth > depth) {
+            leftMostValue = node->val;
+            depth = currDepth;
+        }
+        
+        solve(node->left, currDepth+1, depth, leftMostValue);
+        solve(node->right, currDepth+1, depth, leftMostValue);
+    }
+    
+    int findBottomLeftValue(TreeNode* root) {
+        int depth = 0;
+        int currDepth = 1;
+        int leftMostValue = root->val;
+        
+        solve(root, currDepth, depth, leftMostValue);
+        
+        return leftMostValue;
     }
 };
