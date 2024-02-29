2a6b1edebae7a219afc4ea8dec76bbd1f322fc20
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-29 08:48:32 +0530

    Time: 193 ms (80.26%), Space: 149.3 MB (69.10%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -0,0 +1,114 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        queue<TreeNode*> que;
+        que.emplace(root);
+        
+        int level = 0;
+        int prev;
+        
+        while(!que.empty()) {
+            
+            int n = que.size();
+            
+            for(int i = 0; i < n; i++) {
+                TreeNode* curr = que.front(); que.pop();
+                
+                if(level%2 == 0) { // even level
+
+                    if(i == 0) {
+                        if(curr->val % 2 != 0) {
+                            prev = curr->val;
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                    else {
+                        if(curr->val % 2 != 0 && curr->val > prev) {
+                            prev = curr->val;
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                }
+                else { // odd level
+                    if(i == 0) {
+                        if(curr->val % 2 == 0) {
+                            prev = curr->val;
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                    else {
+                        if(curr->val %2 == 0 && curr->val < prev) {
+                            prev = curr->val;
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                }
+                
+                if(curr->left) {
+                    que.emplace(curr->left);
+                }
+                
+                if(curr->right) {
+                    que.emplace(curr->right);
+                }
+            }
+            
+            level++;
+        }
+        
+        return true;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
