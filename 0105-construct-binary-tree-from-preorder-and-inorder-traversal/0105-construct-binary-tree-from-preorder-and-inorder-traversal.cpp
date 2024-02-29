c883e609dbdc210bcc5410466635fbe2f79a0e64
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-02-29 21:49:09 +0530

    Time: 6 ms (93.82%), Space: 25.4 MB (86.80%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int findElement(vector<int> &arr, int start, int end, int targetElement) {
+        for(int i = start; i <= end; i++) {
+            if(arr[i] == targetElement) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+    
+    TreeNode* buildTreeHelper(vector<int> &pre, vector<int> &in, int size, int &preIndex, int inStart, int inEnd) {
+        // base case
+        if(preIndex >= size || inStart > inEnd) {
+            return nullptr;
+        }
+        
+        // Step A: root
+        int element = pre[preIndex++];
+        TreeNode* root = new TreeNode(element);
+        int pos = findElement(in, inStart, inEnd, element);
+        
+        
+        // Step B: root->left
+        root->left = buildTreeHelper(pre, in, size, preIndex, inStart, pos-1);
+        
+        //Step C: root->right
+        root->right = buildTreeHelper(pre, in, size, preIndex, pos+1, inEnd);
+        
+        return root;
+    }
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        
+        int size = preorder.size();
+        int preIndex = 0;
+        int inStart = 0;
+        int inEnd = size-1;
+        TreeNode* root = buildTreeHelper(preorder, inorder, size, preIndex, inStart, inEnd);
+        
+        return root;
+    }
+};
