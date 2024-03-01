09fa661cadab937b3351d81b897a5182a79928d5
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-01 08:41:10 +0530

    Time: 3 ms (77.44%), Space: 8.4 MB (49.35%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // Approach 1
-    string maximumOddBinaryNumber(string s) {
+    /*string maximumOddBinaryNumber(string s) {
         int n = s.length();
         
         string result = string(n, '0');
@@ -20,5 +20,22 @@ public:
         }
         
         return result;
+    }*/
+    
+    // Approach 2
+    string maximumOddBinaryNumber(string s) {
+        int n = s.length();
+        
+        string result = string(n, '0');
+        
+        int count_1 = count(s.begin(), s.end(), '1');
+        result[n-1] = '1';
+        
+        for(int i = 0; i < count_1-1; i++) {
+            result[i] = '1';
+        }
+        
+        return result;
+        
     }
 };
