738a45a66c717879f4ee910046cb4db74e2b4a83
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-05 23:34:50 +0530

    Time: 3 ms (58.39%), Space: 13.9 MB (55.94%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -44,7 +44,7 @@ public:
     }*/
     
     // Approach 2:- BFS (Without extra space) | T.C:- O(n) | S.C:- O(1)
-    vector<int> rightSideView(TreeNode* root) {
+    /*vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
         if(root == nullptr) {
             return ans;
@@ -73,6 +73,27 @@ public:
                 
             }
         }
+        return ans;
+    }*/
+    
+    // Approach 3:- DFS
+    void solve(TreeNode* root, vector<int> &ans, int level) {
+        if(root == nullptr) {
+            return;
+        }
+        
+        if(level == ans.size()) {
+            ans.push_back(root->val);
+        }
+        
+        solve(root->right, ans, level+1);
+        solve(root->left, ans, level+1);
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        solve(root, ans, 0);
+        
         return ans;
     }
 };
