56239be9bc29a8a28f5a0c5e1aeef2bdb84f8af9
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-06 21:30:57 +0530

    Time: 5 ms (24.83%), Space: 11.9 MB (14.59%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,64 +11,18 @@
  */
 class Solution {
 public:
-    // Using DFS
-    /*bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == nullptr && q == nullptr) return true;
-        if(p == nullptr || q == nullptr) return false;
-        
-        if(p->val != q->val) return false;
-        
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-    }*/
-    
-    // Using BFS
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == nullptr && q == nullptr) return true;
-        
-        if(p == nullptr || q == nullptr) return false;
-        
-        queue<TreeNode*> que1;
-        queue<TreeNode*> que2;
-        
-        que1.push(p);
-        que2.push(q);
-        
-        while(!que1.empty() && !que2.empty()) {
-            TreeNode* node1 = que1.front(); que1.pop();
-            TreeNode* node2 = que2.front(); que2.pop();
-            
-            if(node1->val != node2->val) return false;
-            
-            if(node1->left != nullptr && node2->left != nullptr) {
-                que1.push(node1->left);
-                que2.push(node2->left);
-            }
-            else if(node1->left != nullptr || node2->left != nullptr) {
-                return false;
-            }
-            
-            if(node1->right != nullptr && node2->right != nullptr) {
-                que1.push(node1->right);
-                que2.push(node2->right);
-            }
-            else if(node1->right != nullptr || node2->right != nullptr) {
-                return false;
-            }
-            
+        if(p == nullptr && q == nullptr) {
+            return true;
+        }
+        if(p == nullptr || q == nullptr) {
+            return false;
         }
         
-        return true;
+        if(p->val != q->val) {
+            return false;
+        }
+        
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
