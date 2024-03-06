461be9a79519c6c963088f4eb92b24adec87dfd8
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-06 23:55:03 +0530

    Time: 7 ms (17.16%), Space: 17.7 MB (5.31%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,7 +11,8 @@
  */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
+    // Recursive Solution
+    /*bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p == nullptr && q == nullptr) {
             return true;
         }
@@ -28,5 +29,48 @@ public:
     
     bool isSymmetric(TreeNode* root) {
         return isSameTree(root->left, root->right);
+    }*/
+    
+    // Iteratively
+    bool isMirror(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) return true;
+        if(p == nullptr || q == nullptr) return false;
+        
+        queue<TreeNode*> que1;
+        queue<TreeNode*> que2;
+        
+        que1.push(p);
+        que2.push(q);
+        
+        while(!que1.empty() && !que2.empty()) {
+            TreeNode* node1 = que1.front(); que1.pop();
+            TreeNode* node2 = que2.front(); que2.pop();
+            
+            if(node1->val != node2->val) {
+                return false;
+            }
+            
+            if(node1->left != nullptr && node2->right != nullptr) {
+                que1.push(node1->left);
+                que2.push(node2->right);
+            }
+            else if(node1->left != nullptr || node2->right != nullptr) {
+                return false;
+            }
+            
+            if(node1->right != nullptr && node2->left != nullptr) {
+                que1.push(node1->right);
+                que2.push(node2->left);
+            }
+            else if(node1->right != nullptr || node2->left != nullptr) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        return isMirror(root->left, root->right);
     }
 };
