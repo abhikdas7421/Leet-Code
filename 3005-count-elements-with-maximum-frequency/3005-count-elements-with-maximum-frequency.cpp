4285f3a102e8308b41fd77de6694db3d3955bd98
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-10 00:18:14 +0530

    Time: 3 ms (84.13%), Space: 21.8 MB (22.34%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
-    int maxFrequencyElements(vector<int>& nums) {
+    // Approach 2:- (2 pass) | T.C:- O(n+n) | S.C:- O(1)
+    /*int maxFrequencyElements(vector<int>& nums) {
         vector<int> freq(101);
+        
+        for(int i = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
+        }
         
         int maxFreq = 0;
         int maxFreqCount = 0;
         
-        for(int i = 0; i < nums.size(); i++) {
-            freq[nums[i]]++;
-            
-            if(maxFreq < freq[nums[i]]) {
-                maxFreq = freq[nums[i]];
+        for(int i = 0; i < freq.size(); i++) {
+            if(maxFreq < freq[i]) {
+                maxFreq = freq[i];
                 maxFreqCount = 1;
             }
-            else if(maxFreq == freq[nums[i]]) {
+            else if(maxFreq == freq[i]) {
                 maxFreqCount++;
             }
         }
         
         int result = maxFreq*maxFreqCount;
         return result;
+    }*/
+    
+    // Appraoch 3:- (1 pass) | T.C:- O(n) | S.C:- O(1)
+    int maxFrequencyElements(vector<int>& nums) {
+        vector<int> freq(101);
+        
+        int maxFreq = 0;
+        int total = 0;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
+            
+            if(maxFreq < freq[nums[i]]) {
+                maxFreq = freq[nums[i]];
+                total = maxFreq;
+            }
+            else if(maxFreq == freq[nums[i]]) {
+                total += maxFreq;
+            }
+        }
+        
+        return total;
     }
 };
