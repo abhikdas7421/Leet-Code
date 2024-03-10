2cf6685a7190002daaf927b96e9803d99923f3ee
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-10 11:51:57 +0530

    Time: 4 ms (68.61%), Space: 12.5 MB (61.63%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> h(1000, 0);
         vector<int> ans;
         
-        for(int i = 0; i < nums1.size(); i++) {
-            
-            if(h[nums1[i]] == 0) {
-                h[nums1[i]]++;
-            }
-            
-        }
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
         
-        for(int i = 0; i < nums2.size(); i++) {
-            if(h[nums2[i]] != 0) {
-                ans.push_back(nums2[i]);
-                h[nums2[i]]--;
+        int i = 0, j = 0;
+        
+        while(i < nums1.size() && j < nums2.size()) {
+            
+            if(nums1[i] == nums2[j]) {
+                if(ans.size() == 0 || (ans.size() != 0 && ans[ans.size()-1] != nums1[i])) {
+                    ans.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+            else if(nums1[i] < nums2[j]) {
+                i++;
+            }
+            else if(nums1[i] > nums2[j]) {
+                j++;
             }
         }
         
         return ans;
-        
     }
 };
