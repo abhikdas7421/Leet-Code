a84a57177d1ef3ccdd085ce0c322f98cec84e1db
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-16 22:54:04 +0530

    Time: 115 ms (14.01%), Space: 61.2 MB (41.82%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -7,8 +7,23 @@ public:
     }*/
     
     // Approach 2:- Sort Acending order | T.C:- O(nlogn)
-    int findKthLargest(vector<int>& nums, int k) {
+    /*int findKthLargest(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         return nums[nums.size()-k];
+    }*/
+    
+    // Approach 3:- Using Min Heap
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq; // default max heap
+        
+        for(int &num : nums) {
+            pq.push(num);
+            
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+        
+        return pq.top();
     }
 };
