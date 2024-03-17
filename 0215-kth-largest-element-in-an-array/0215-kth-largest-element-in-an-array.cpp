4f206be98b2a20b3b1354cf5e52603a7ac4e112c
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-17 13:08:23 +0530

    Time: 93 ms (60.92%), Space: 57.7 MB (92.42%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -13,7 +13,7 @@ public:
     }*/
     
     // Approach 3:- Using Min Heap
-    int findKthLargest(vector<int>& nums, int k) {
+    /*int findKthLargest(vector<int>& nums, int k) {
         priority_queue<int, vector<int>, greater<int>> pq; // default max heap
         
         for(int &num : nums) {
@@ -25,5 +25,61 @@ public:
         }
         
         return pq.top();
+    }*/
+    
+    // Approach 4:- Quick Select
+    int partition_algo(vector<int>& nums, int L, int R) {
+        
+        int p = nums[L];
+        int i = L+1;
+        int j = R;
+        
+        while(i <= j) {
+            
+            if(nums[i] < p && nums[j] > p) {
+                swap(nums[i], nums[j]);
+                
+                i++;
+                j--;
+            }
+            
+            if(nums[i] >= p) {
+                i++;
+            }
+            
+            if(nums[j] <= p) {
+                j--;
+            }
+        }
+        
+        swap(nums[j], nums[L]);
+        return j;
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        int L = 0;
+        int R = n-1;
+        
+        int pivot_idx = 0;
+        
+        while(true) {
+            
+            pivot_idx = partition_algo(nums, L, R);
+            
+            if(pivot_idx == k-1) {
+                break;
+            }
+            else if(pivot_idx > k-1) {
+                R = pivot_idx-1;
+            }
+            else {
+                L = pivot_idx+1;
+            }
+        }
+        
+        return nums[pivot_idx];
+
     }
 };
