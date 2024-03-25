19918a1d7c21369963f1fb02796f984ce6b90d8b
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-25 21:17:48 +0530

    Time: 11 ms (80.74%), Space: 20.4 MB (60.12%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,50 @@
 class Solution {
 public:
+    // Approach:- 1(Math)
+    /*int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = (n * (n+1))/2;
+
+        int arraySum = 0;
+        for(int &num : nums) {
+            arraySum += num;
+        }
+
+        int missingNum = totalSum-arraySum;
+        return missingNum;
+    }*/
+
+    // Approach:- 2(bit manupulation)
     int missingNumber(vector<int>& nums) {
         int ans = 0;
-        for(auto i : nums) {
-            ans ^= i;
+        for(int &num : nums) {
+            ans ^= num;
         }
 
         for(int i = 0; i <= nums.size(); i++) {
             ans ^= i;
         }
-        
+
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
