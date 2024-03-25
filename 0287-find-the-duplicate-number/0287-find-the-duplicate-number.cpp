a495e7653982ee8feccab8cfae7643d386d27e13
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-25 20:52:04 +0530

    Time: 82 ms (55.79%), Space: 63.4 MB (93.61%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
-    // Approach:- 1 (Array)
+    // Approach:- 2(Hair and Tortoise)
     int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
 
-        while (nums[0] != nums[nums[0]]) {
-            swap(nums[0], nums[nums[0]]);
+        // Detect cycle
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
         }
 
-        return nums[0];
+        slow = 0;
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
     }
 };
