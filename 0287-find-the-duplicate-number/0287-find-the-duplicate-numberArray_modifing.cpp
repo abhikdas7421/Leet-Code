57df84fd37e5c8c501f19745808d9ea06eee3ac5
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-25 20:54:53 +0530

    Update 0287-find-the-duplicate-numberArray_modifing.cpp

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-numberArray_modifing.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-numberArray_modifing.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
-    // Approach:- 2(Hair and Tortoise)
+    // Approach:- 1
     int findDuplicate(vector<int>& nums) {
-        int slow = nums[0];
-        int fast = nums[nums[0]];
-
-        // Detect cycle
-        while(slow != fast) {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        }
-
-        slow = 0;
-        while(slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-
-        return slow;
+        while(nums[0] != nums[nums[0]]) {
+            swap(nums[0], nums[nums[0]]);
     }
+    return nums[0]
 };
