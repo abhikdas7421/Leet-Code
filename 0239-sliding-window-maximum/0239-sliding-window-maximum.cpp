48a0ef28be4e79dfabb23cdad2471c5dfebaccd7
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-28 00:39:16 +0530

    Time: 173 ms (60.03%), Space: 137 MB (71.98%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
         vector<int> ans;
+        deque<int> dq;
         
-        // process first k window
         for(int i = 0; i < k; i++) {
             
-            // Step 1:-  remove smaller element than curr element
             while(!dq.empty() && nums[i] >= nums[dq.back()]) {
                 dq.pop_back();
             }
             
-            // Step 2:- push_back curr element
             dq.push_back(i);
         }
         
-        // Step 3:- store answer for first window
         ans.push_back(nums[dq.front()]);
         
-        // Process remaining windows
-        for(int i = k; i < nums.size(); i++) {
+        int i = 0;
+        int j = k;
+        
+        while(j < nums.size()) {
             
-            // Step 1:- remove outof bound element from dq
-            if(!dq.empty() && i-dq.front() >= k) {
+            while(!dq.empty() && dq.front() <= i) {
                 dq.pop_front();
             }
             
-            // Step 2:- remove smaller element than curr element
-            while(!dq.empty() && nums[i] >= nums[dq.back()]) {
+            while(!dq.empty() && nums[j] >= nums[dq.back()]) {
                 dq.pop_back();
             }
             
-            // Step 3:- push_back curr element
-            dq.push_back(i);
+            dq.push_back(j);
             
-            // Step 4:- stroe answer for current window
             ans.push_back(nums[dq.front()]);
+            
+            i++;
+            j++;            
         }
         
         return ans;
