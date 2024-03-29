8b1b948bd734073c682a0dfab270c1b76b8083f2
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-03-29 23:07:16 +0530

    Time: 212 ms (57.78%), Space: 148.4 MB (86.23%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int maxSubarrayLength(vector<int>& nums, int k) {
+    // Approach:- 1 | T.C:- O(n) | S.C:- O(1)
+    /*int maxSubarrayLength(vector<int>& nums, int k) {
         int n = nums.size(), i = 0, j = 0, ans = 0;
+        
         unordered_map<int, int> freq;
         
         while(j < n) {
             freq[nums[j]]++;
             
-            while(freq[nums[j]] > k) {
+            while(i < j && freq[nums[j]] > k) {
                 freq[nums[i]]--;
                 i++;
             }
@@ -17,7 +19,39 @@ public:
         }
         
         return ans;
+    }*/
+    
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
         
+        int i = 0;
+        int j = 0;
+        int result = 0;
         
+        int culprit = 0;
+        
+        while(j < n) {
+            mp[nums[j]]++;
+            
+            if(mp[nums[j]] == k+1) {
+                culprit++;
+            }
+            
+            if(culprit > 0) {
+                mp[nums[i]]--;
+                if(mp[nums[i]] == k) {
+                    culprit--;
+                }
+                i++;
+            }
+            
+            if(culprit == 0) {
+                result = max(result, j-i+1);
+            }
+            j++;
+        }
+        
+        return result;
     }
 };
