8e83c5096ec9e676520c7a527e48e64aaf29fdd3
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-04-14 21:04:43 +0530

    Time: 3 ms (37.24%), Space: 8.1 MB (53.07%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,51 +1,52 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        int total_size = rows*cols;
-
-        int count = 0;
-
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int totalSize = row*col;
+        
         int startingRow = 0;
-        int endingRow = rows-1;
+        int endingRow = row-1;
+        
         int startingCol = 0;
-        int endingCol = cols-1;
-
-        while(count < total_size) {
+        int endingCol = col-1;
+        
+        int count = 0;
+        vector<int> ans;
+        
+        while(count < totalSize) {
+            
             // starting row
-            for(int i = startingCol; i <= endingCol && count < total_size; i++) {
+            for(int i = startingCol; i <= endingCol && count < totalSize; i++) {
                 ans.push_back(matrix[startingRow][i]);
                 count++;
             }
             startingRow++;
-
-
+            
+            
             // ending col
-            for(int i = startingRow; i <= endingRow && count < total_size; i++) {
+            for(int i = startingRow; i <= endingRow && count < totalSize; i++) {
                 ans.push_back(matrix[i][endingCol]);
                 count++;
             }
             endingCol--;
-
-
+            
+            
             // ending row
-            for(int i = endingCol; i >= startingCol && count < total_size; i--) {
+            for(int i = endingCol; i >= startingCol && count < totalSize; i--) {
                 ans.push_back(matrix[endingRow][i]);
                 count++;
             }
             endingRow--;
-
-
+            
             // starting col
-            for(int i = endingRow; i >= startingRow && count < total_size; i--) {
+            for(int i = endingRow; i >= startingRow && count < totalSize; i--) {
                 ans.push_back(matrix[i][startingCol]);
                 count++;
             }
             startingCol++;
         }
-
+        
         return ans;
         
     }
