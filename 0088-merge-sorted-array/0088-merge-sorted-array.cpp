8e03bd44e13f79d061c338b3130997b7ae12104c
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-04-17 11:10:26 +0530

    Time: 6 ms (8.28%), Space: 11.1 MB (6.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,73 +1,32 @@
 class Solution {
 public:
-    // Approach 1:- Using extra space | T.C -> O(n+m) | S.C -> O(n+m)
-    /*void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> mergedArr(n+m);
+    // Approach:- 1
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> mergedArray;
         
-        int i = 0; // for nums1
-        int j = 0; // for nums2
-        int k = 0; // for mergedArr
+        int i = 0, j = 0;
         
         while(i < m && j < n) {
             if(nums1[i] < nums2[j]) {
-                mergedArr[k] = nums1[i];
+                mergedArray.push_back(nums1[i]);
                 i++;
             }
             else {
-                mergedArr[k] = nums2[j];
+                mergedArray.push_back(nums2[j]);
                 j++;
             }
-            k++;
         }
         
         while(i < m) {
-            mergedArr[k++] = nums1[i++];
+            mergedArray.push_back(nums1[i]);
+            i++;
         }
         
         while(j < n) {
-            mergedArr[k++] = nums2[j++];
+            mergedArray.push_back(nums2[j]);
+            j++;
         }
         
-        i = 0; k = 0;
-        
-        while(k < n+m) {
-            nums1[k] = mergedArr[k];
-            k++;
-        }
-        
-    }*/
-    
-    // Approach 2:- Sorting | T.C -> O((n+m)log(n+m)) | S.C -> O(1)
-    /*void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i = m, j = 0; j < n; i++, j++) {
-            nums1[i] = nums2[j];
-        }
-        
-        sort(nums1.begin(), nums1.end());
-    }*/
-    
-    // Approach 3:- Two Pointer | T.C -> O(n+m) | S.C -> O(1)
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        int i = m-1; // for nums1
-        int j = n-1; // for nums2
-        int k = m+n-1;
-        
-        while(i >= 0 && j >= 0) {
-            if(nums2[j] > nums1[i]) {
-                nums1[k] = nums2[j];
-                j--;
-            }
-            else {
-                nums1[k] = nums1[i];
-                i--;
-            }
-            
-            k--;
-        }
-        
-        while(j >= 0) {
-            nums1[k--] = nums2[j--];
-        }
+        nums1 = mergedArray;
     }
 };
