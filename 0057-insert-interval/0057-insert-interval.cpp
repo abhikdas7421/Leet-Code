f169eaf843d75a97df0714e6cccf70d25443fcb0
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-04-17 12:33:57 +0530

    Time: 20 ms (8.29%), Space: 20.6 MB (26.60%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,27 +1,5 @@
 class Solution {
 public:
-    // Approach 1:- T.C:- O(nlogn)
-    /*vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(), intervals.end()); // O(nlogn)
-        
-        int n = intervals.size();
-        vector<vector<int>> ans;
-        
-        for(int i = 0; i < n; i++) {
-            
-            if(ans.empty() || ans.back()[1] < intervals[i][0]) {
-                ans.push_back(intervals[i]);
-            }
-            else {
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-            }
-        }
-        
-        return ans;
-    }*/
-    
-    // Approach 2:- T.C:- O(n)
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n = intervals.size();
         
@@ -29,6 +7,7 @@ public:
         
         int i = 0;
         while(i < n) {
+            
             if(intervals[i][1] < newInterval[0]) {
                 ans.push_back(intervals[i]);
             }
@@ -39,6 +18,7 @@ public:
                 newInterval[0] = min(newInterval[0], intervals[i][0]);
                 newInterval[1] = max(newInterval[1], intervals[i][1]);
             }
+            
             i++;
         }
         
@@ -52,11 +32,3 @@ public:
         return ans;
     }
 };
-    
-    
-    
-    
-    
-    
-    
-    
