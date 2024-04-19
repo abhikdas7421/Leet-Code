c040b4bfab4043281394e3adefa8e7a37cb317bd
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-04-19 11:43:25 +0530

    Time: 24 ms (14.48%), Space: 30.3 MB (22.57%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -4,7 +4,8 @@ public:
     int di[4] = {-1, 0, 0, 1};
     int dj[4] = {0, -1, 1, 0};
     
-    void dfs(vector<vector<int>>& grid, int i, int j, int& area) {
+    // Approach:- 1 (Using DFS) | T.C:- O(n*m) | S.C:- O(1)
+    /*void dfs(vector<vector<int>>& grid, int i, int j, int& area) {
         
         if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) {
             return;
@@ -35,6 +36,63 @@ public:
                     int currArea = 0;
                     dfs(grid, i, j, currArea);
                     
+                    maxArea = max(maxArea, currArea);
+                }
+            }
+        }
+        
+        return maxArea;
+    }*/
+    
+    // Approach:- 2 (Using BFS)
+    
+    void bfs(vector<vector<int>>& grid, int i, int j, int& area) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        // int area = 0;
+        
+        queue<pair<int, int>> que;
+        que.push({i, j});
+        grid[i][j] = -1;
+        area++;
+        
+        while(!que.empty()) {
+            
+            pair<int, int> p = que.front();
+            que.pop();
+            
+            for(int k = 0; k < 4; k++) {
+                int pi = p.first + di[k];
+                int pj = p.second + dj[k];
+                
+                if(pi < 0 || pi >= n || pj < 0 || pj >= m || grid[pi][pj] == 0 || grid[pi][pj] == -1) {
+                    continue;
+                }
+                else if(grid[pi][pj] == 1) {
+                    grid[pi][pj] = -1;
+                    area++;
+                    que.push({pi, pj});
+                }
+            }
+        }
+        
+    }
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        int maxArea = 0;
+        
+        for(int i = 0; i < n; i++) {
+            
+            for(int j = 0; j < m; j++) {
+                
+                if(grid[i][j] == 1) {
+                    
+                    int currArea = 0;
+                    bfs(grid, i, j, currArea);
                     maxArea = max(maxArea, currArea);
                 }
             }
