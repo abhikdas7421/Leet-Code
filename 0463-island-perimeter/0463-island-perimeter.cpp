32c0bd30df4b506bc3a61e7a9b93099fa5cd8cb0
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-04-19 10:12:37 +0530

    Time: 81 ms (36.31%), Space: 104.5 MB (12.78%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -39,8 +39,8 @@ public:
         return perimeter; 
     }*/
     
-    // Approach:- 2 (Using DFS)
-    int di[4] = {-1, 0, 0, 1};
+    // Approach:- 2 (Using DFS) | T.C:- O(n*m) | S.C:- O(1)
+    /*int di[4] = {-1, 0, 0, 1};
     int dj[4] = {0, -1, 1, 0};
     
     void dfs(vector<vector<int>>& grid, int i, int j, int &peri) {
@@ -78,5 +78,60 @@ public:
         }
         
         return -1;
+    }*/
+    
+    // Approach:- 3 (Using BFS) | T.C:- O(n*m) | S.C:- O(n*m)
+    
+    int di[4] = {-1, 0, 0, 1};
+    int dj[4] = {0, -1, 1, 0};
+    
+    void bfs(vector<vector<int>>& grid, int i, int j, int& peri) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        queue<pair<int, int>> que;
+        que.push({i, j});
+        grid[i][j] = -1; // mark visited
+        
+        while(!que.empty()) {
+            pair<int, int> p = que.front();
+            que.pop();
+            
+            
+            
+            for(int k = 0; k < 4; k++) {
+                int pi = p.first + di[k];
+                int pj = p.second + dj[k];
+                
+                if(pi < 0 || pi >= n || pj < 0 || pj >= m || grid[pi][pj] == 0) {
+                    peri++;
+                }
+                else if(grid[pi][pj] == 1) {
+                    grid[pi][pj] = -1;
+                    que.push({pi, pj});
+                }
+            }
+        }
+    }
+    
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        int peri = 0;
+        
+        for(int i = 0; i < n; i++) {
+            
+            for(int j = 0; j < m; j++) {
+                
+                if(grid[i][j] == 1) {
+                    bfs(grid, i, j, peri);
+                    return peri;
+                }
+            }
+        }
+        
+        return -1;
+        
     }
 };
