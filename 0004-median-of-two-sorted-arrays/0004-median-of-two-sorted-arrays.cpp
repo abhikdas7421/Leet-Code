ad8b4acd462607a3fe30fdba4af275e2dd005aa6
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-04-21 22:32:09 +0530

    Time: 24 ms (63.39%), Space: 94.5 MB (62.05%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,10 +1,11 @@
 class Solution {
-    void merge(vector<int>& arr1, vector<int>& arr2) { 
+public:
+    // Approach:- 1 | T.C:- O(nlogn + mlogm) | S.C:- O(1)
+    /*void merge(vector<int>& arr1, vector<int>& arr2) { 
         int n = arr1.size();
         int m = arr2.size();
         
-        int i = n-1;
-        int j = 0;
+        int i = n-1, j = 0;
         
         while(i >= 0 && j < m) { // O(n)
             if(arr1[i] > arr2[j]) {
@@ -19,22 +20,14 @@ class Solution {
         
         sort(arr1.begin(), arr1.end()); //O(nlogn)
         sort(arr2.begin(), arr2.end()); //O(mlogm)
-        
-        
     }
     
-public:
     double findMedianSortedArrays(vector<int>& array1, vector<int>& array2) {
         int n = array1.size();
         int m = array2.size();
         int total_size = n+m;
         
         merge(array1, array2);
-        
-        // for(int &i : array1) cout << i << " ";
-        // cout << endl;
-        // for(int &i : array2) cout << i << " ";
-        // cout << endl;
         
         if(total_size&1 == 1) { // odd size
             int mid = total_size/2;
@@ -60,5 +53,81 @@ public:
             }
         }
         
+    }*/
+    
+    // Approach:- 2 | T.C:- O(n)
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        
+        int total_size = n+m;
+        int mid = total_size/2;
+        
+        int element1 = 0;
+        int element2 = 0;
+        
+        int i = 0, j = 0, k = 0;
+        
+        while(i < n && j < m) {
+            
+            if(nums1[i] < nums2[j]) {
+                
+                if(k == mid-1) {
+                    element1 = nums1[i];
+                }
+                
+                if(k == mid) {
+                    element2 = nums1[i];
+                }
+                
+                i++;
+            }
+            else  {
+                if(k == mid-1) {
+                    element1 = nums2[j];
+                }
+                
+                if(k == mid) {
+                    element2 = nums2[j];
+                }
+                
+                j++;
+            }
+            
+            k++;
+        }
+        
+        while(i < n) {
+            if(k == mid-1) {
+                    element1 = nums1[i];
+                }
+                
+            if(k == mid) {
+                element2 = nums1[i];
+            }
+            
+            i++;
+            k++;
+        }
+        
+        while(j < m) {
+            if(k == mid-1) {
+                    element1 = nums2[j];
+                }
+                
+            if(k == mid) {
+                element2 = nums2[j];
+            }
+                
+            j++;
+            k++;
+        }
+        
+        if(total_size & 1 == 1) {
+            return element2;
+        }
+        else {
+            return (element1 + element2)/2.0;
+        }
     }
 };
