f7cc920a7c0c41683f938ea8af4be45530058997
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-04-22 21:19:21 +0530

    Time: 24 ms (16.12%), Space: 26.6 MB (91.11%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -31,17 +31,18 @@ public:
     
     // Approach 3:- Moore's Voting Algorithm | T.C -> O(n) | S.C -> O(1)
     int majorityElement(vector<int>& nums) {
-        int count = 0;
-        int candidate = 0; // random number
+        int n = nums.size();
         
-        for(int num : nums) {
+        int count = 0;
+        int candidate = 0;
+        
+        for(int &num : nums) {
             
             if(count == 0) {
                 candidate = num;
             }
             
             count += (candidate == num) ? 1 : -1;
-            
         }
         
         return candidate;
