48dab078f155fd4df5fc1c8f62f5cb75afcb838a
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-05-02 00:37:02 +0530

    Time: 87 ms (72.42%), Space: 57.7 MB (92.67%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,85 +1,38 @@
 class Solution {
 public:
-    // Approach 1:- Sort Decending order | T.C:- O(nlogn)
-    /*int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end(), greater<int>());
-        return nums[k-1];
-    }*/
-    
-    // Approach 2:- Sort Acending order | T.C:- O(nlogn)
-    /*int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()-k];
-    }*/
-    
-    // Approach 3:- Using Min Heap
-    /*int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq; // default max heap
-        
-        for(int &num : nums) {
-            pq.push(num);
-            
-            if(pq.size() > k) {
-                pq.pop();
-            }
-        }
-        
-        return pq.top();
-    }*/
-    
-    // Approach 4:- Quick Select | T.C:- O(n) avg, O(n*n) worst case
-    int partition_algo(vector<int>& nums, int L, int R) {
-        
-        int p = nums[L];
-        int i = L+1;
-        int j = R;
+    int partition_algo(vector<int>& nums, int l, int r) {
+        int pivot = nums[l];
+        int i = l+1;
+        int j = r;
         
         while(i <= j) {
             
-            if(nums[i] < p && nums[j] > p) {
-                swap(nums[i], nums[j]);
+            if(pivot > nums[i] && pivot < nums[j]) swap(nums[i++], nums[j--]);
                 
-                i++;
-                j--;
-            }
+            if(nums[i] >= pivot) i++;
             
-            if(nums[i] >= p) {
-                i++;
-            }
+            if(nums[j] <= pivot) j--;
             
-            if(nums[j] <= p) {
-                j--;
-            }
         }
         
-        swap(nums[j], nums[L]);
+        swap(nums[l], nums[j]);
         return j;
     }
     
     int findKthLargest(vector<int>& nums, int k) {
         int n = nums.size();
         
-        int L = 0;
-        int R = n-1;
-        
-        int pivot_idx = 0;
+        int l = 0;
+        int r = n-1;
         
         while(true) {
+            int pivot_idx = partition_algo(nums, l, r);
             
-            pivot_idx = partition_algo(nums, L, R);
-            
-            if(pivot_idx == k-1) {
-                break;
-            }
-            else if(pivot_idx > k-1) {
-                R = pivot_idx-1;
-            }
-            else {
-                L = pivot_idx+1;
-            }
+            if(pivot_idx == k-1) break;
+            else if(pivot_idx > k-1) r = pivot_idx-1;
+            else l = pivot_idx+1;
         }
         
-        return nums[pivot_idx];
-
+        return nums[k-1];
     }
 };
