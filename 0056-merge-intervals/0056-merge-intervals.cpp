ee818c1423438bfd2c80ea2f8bb3c52a445d590e
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-05-01 21:05:43 +0530

    Time: 25 ms (46.65%), Space: 22.8 MB (42.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -11,7 +11,7 @@ public:
             if(ans.empty() || ans.back()[1] < intervals[i][0]) {
                 ans.push_back(intervals[i]);
             }
-            else{
+            else{  // ans.back()[1] >= intervals[i][0]
                 ans.back()[0] = min(ans.back()[0], intervals[i][0]);
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
