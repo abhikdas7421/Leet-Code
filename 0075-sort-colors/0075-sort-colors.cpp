edd8c5ea6862103c9d5fddab2a7e1e44c7470b30
ABHIK DAS <79216907+abhikdas7421@users.noreply.github.com>
2024-05-02 00:48:59 +0530

    Time: 2 ms (43.20%), Space: 9.9 MB (33.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,44 +1,12 @@
 class Solution {
 public:
-    // Approach:- 1 (counting)
-    /*void sortColors(vector<int>& nums) {
-        int zeros = 0, ones = 0, twos= 0;
-        
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == 0) {
-                zeros++;
-            }
-            else if(nums[i] == 1) {
-                ones++;
-            }
-            else if(nums[i] == 2) {
-                twos++;
-            }
-        }
-        
-        int i = 0;
-        while(zeros--) {
-            nums[i++] = 0;
-        }
-        
-        while(ones--) {
-            nums[i++] = 1;
-        }
-        
-        while(twos--) {
-            nums[i++] = 2;
-        }
-    }*/
-    
-    // Approach:- 2 (Dutch national flag algorithm)
-    void sortColors(vector<int>& nums) { 
+    void sortColors(vector<int>& nums) {
         int n = nums.size();
         
-        int l = 0;
-        int m = 0;
-        int h = n-1;
+        int l = 0, m = 0, r = n-1;
         
-        while(m <= h) {
+        while(m <= r) {
+            
             if(nums[m] == 0) {
                 swap(nums[l], nums[m]);
                 l++;
@@ -48,9 +16,10 @@ public:
                 m++;
             }
             else {
-                swap(nums[m], nums[h]);
-                h--;
+                swap(nums[m], nums[r]);
+                r--;
             }
         }
+        
     }
 };
